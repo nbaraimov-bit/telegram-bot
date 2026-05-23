2187ece10625e180124d541b1786b4a2c7488688
Nurmuhammad <nbaraimov@gmail.com>
2026-05-23 23:46:25 +0500

    restart telegram

--- bot.js
+++ bot.js
@@ -313,3 +313,17 @@ bot.sendMessage(
   5793538486,
   "Test xabar 🚀"
 )
+
+import express from "express"
+
+const app = express()
+
+app.get("/", (req, res) => {
+  res.send("Bot ishlayapti")
+})
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+  console.log("Server started")
+})
